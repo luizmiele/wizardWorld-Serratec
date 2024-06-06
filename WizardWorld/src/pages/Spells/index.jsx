759529ef3073a4ef56a1b99048254a1cbb97971c
Cássio <penhacassio@gmail.com>
2024-06-06 20:40:48 -0300

    Feat: Troca favicon

---
+++ WizardWorld/src/pages/Spells/index.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import moldura from "../../assets/images/enzo-diretor-moldura.png";
+import sala from "../../assets/images/Sala.webp";
+import styles from "./styles.module.css"
+
+const Spells = () => {
+  // const [NomePocao, setNomePocao] = useState("");
+  // const [receita, setReceita] = useState("");
+
+  // const pesquisar = (e) => {
+  //   // Função para lidar com a busca da poção
+  //   e.preventDefault();
+  //   // Aqui você pode adicionar a lógica para buscar a receita da poção
+  //   setReceita(`Receita da nome da receita`); // Apenas um exemplo
+  // };
+
+  return (
+    <div className={styles.app}>
+      <img src={sala} alt="plano de fundo mostrando uma sala com feitiços" />
+      <header className={styles.header}>
+        <p>Olá estudante! Qual feitiço você gostaria de pesquisar?</p>
+      </header>
+      <main className={styles.main}>
+        <div className={styles.searchSection}>
+          <h2>Qual feitiço está procurando?</h2>
+          <form>
+            <input
+              type="text"
+              placeholder="Procure sua poção..."
+            />
+            <button type="submit">🔍</button>
+          </form>
+        </div>
+      </main>
+      <aside className={styles.aside}>
+        <img src={moldura} alt="Professor" />
+      </aside>
+    </div>
+  );
+};
+
+export default Spells;
