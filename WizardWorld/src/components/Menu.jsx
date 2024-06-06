c6be1a3ecbed13a5fb277257b5e01face522563f
Victor Siqueira <vg_siqueira@hotmail.com>
2024-06-05 21:39:37 -0300

    style: update icons in menu

--- WizardWorld/src/components/Menu.jsx
+++ WizardWorld/src/components/Menu.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Menu.module.css';
-import potionIcon from "../assets/images/icone-temporario.jpg";
-import spellIcon from "../assets/images/icone-temporario.jpg";
-import quidditchIcon from "../assets/images/icone-temporario.jpg";
-import creditsIcon from "../assets/images/icone-temporario.jpg";
+import potionIcon from "../assets/images/icone-temporario.png";
+import spellIcon from "../assets/images/icone-temporario.png";
+import quidditchIcon from "../assets/images/icone-temporario.png";
+import creditsIcon from "../assets/images/icone-temporario.png";
 
 const Menu = () => {
   return (
     <nav className={styles.menu}>
       <ul>
-        <div className= {styles.pocoes}><li><img src={potionIcon} alt="Potions" /><Link to="/potions">Poções</Link></li></div>
-        <div className= {styles.feiticos}><li><img src={spellIcon} alt="Spells" /><Link to="/spells">Feitiços</Link></li></div>
-        <div className= {styles.quadribol}><li><img src={quidditchIcon} alt="Quidditch" /><Link to="/quidditch">Quadribol</Link></li></div>
-        <div className= {styles.creditos}><li><img src={creditsIcon} alt="Credits" /><Link to="/credits">Créditos</Link></li></div>
+      <div className={styles.pocoes}><li><Link to="/potions"><img src={potionIcon} alt="Pocões" /></Link></li></div>
+      <div className={styles.feiticos}><li><Link to="/spells"><img src={spellIcon} alt="Feitiços" /></Link></li></div>
+      <div className={styles.quadribol}><li><Link to="/quidditch"><img src={quidditchIcon} alt="Quadribol" /></Link></li></div>
+      <div className={styles.creditos}><li><Link to="/credits"><img src={creditsIcon} alt="Créditos" /></Link></li></div>
       </ul>
     </nav>
   );
