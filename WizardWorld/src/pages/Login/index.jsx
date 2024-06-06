d8d716cbcae4cc596dc0445267a5805fe884201f
luiz miele <luiz.miele@hotmail.com>
2024-06-06 14:40:01 -0300

    feat: Login functional

--- WizardWorld/src/pages/Login/index.jsx
+++ WizardWorld/src/pages/Login/index.jsx
@@ -1,54 +1,101 @@
-import React, { useState } from 'react';
-import styles from './styles.module.css';
+import React, { useState, useEffect } from "react";
+import styles from "./styles.module.css";
 import { FaUser, FaLock } from "react-icons/fa";
-import Wizarding_World_Logo from '../../assets/images/Wizarding_World_Logo.png'
-import { useNavigate } from 'react-router-dom';
-
+import Wizarding_World_Logo from "../../assets/images/Wizarding_World_Logo.png";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const [checaLogin, setChecaLogin] = useState(0);
+  const [house, setHouse] = useState("");
 
-    const handleLogin = () => {
-          navigate(`/slytherin`);
-      };
+  const users = [
+    { email: "alunoG", password: "123", house: "gryffindor" },
+    { email: "alunoS", password: "1234", house: "slytherin" },
+    { email: "alunoH", password: "12345", house: "hufflepuff" },
+    { email: "alunoR", password: "123456", house: "ravenclaw" },
+  ];
 
-    return (
-        <div className={styles.principal}>
-            <div className={styles.wwlogo}>
-                    <a className= {styles.model}href="https://www.wizardingworld.com/" target="_blank">
-                        <img src={Wizarding_World_Logo} alt="Wizarding World Logo" />
-                    </a>
-                </div>
-            <div className={styles.wrapper}>
-                
-                <form action="">
-                    <h1>Login</h1>
-                    <div className={styles.inputBox}>
-                        <input type="text" placeholder='E-mail' required />
-                        <FaUser className={styles.icon}/>
-                    </div>
-                    <div className={styles.inputBox}>
-                        <input type="password" placeholder='Senha' required />
-                        <FaLock className={styles.icon}/>
-                    </div>
+  useEffect(() => {
+    const user = users.find((user) => user.email === email && user.password === password);
+    if (checaLogin > 0) {
+      if (user) {
+        handleHouse(user.house);
+      } else {
+        alert("login invalido!");
+      }
+    }
+  }, [checaLogin]);
 
-                
-                    <div className={styles.rememberForgot}>
-                        <label><input type="checkbox" />Lembrar-me</label>
-                        <a href="#">Esqueceu a senha?</a>
-                    </div>
-                
-                    <button type='submit' onClick={handleLogin}>Entrar em sessão</button>
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
 
-                    <div className={styles.registerLink}>
-                        <p>Não tem uma conta?</p>
-                    </div>
-                    <button className={styles.submit}>Inscrever-se</button>
+  function handleSenha(e) {
+    setPassword(e.target.value);
+  }
 
-                </form>
-            </div>
-        </div>
-    );
+  const handleLogin = () => {
+      setChecaLogin(checaLogin + 1);
+  };
+
+  const handleHouse = (house) => {
+    navigate(`/${house}`);
+  }
+
+  return (
+    <div className={styles.principal}>
+      <div className={styles.wwlogo}>
+        <a
+          className={styles.model}
+          href="https://www.wizardingworld.com/"
+          target="_blank"
+        >
+          <img src={Wizarding_World_Logo} alt="Wizarding World Logo" />
+        </a>
+      </div>
+      <div className={styles.wrapper}>
+        <form action="">
+          <h1>Login</h1>
+          <div className={styles.inputBox}>
+            <input type="text" 
+            placeholder="E-mail" 
+            value={email}
+            onChange={handleEmail}
+            required />
+            <FaUser className={styles.icon} />
+          </div>
+          <div className={styles.inputBox}>
+            <input type="password" 
+            placeholder="Senha" 
+            value={password}
+            onChange={handleSenha}
+            required />
+            <FaLock className={styles.icon} />
+          </div>
+
+          <div className={styles.rememberForgot}>
+            <label>
+              <input type="checkbox" />
+              Lembrar-me
+            </label>
+            <a href="#">Esqueceu a senha?</a>
+          </div>
+
+          <button type="submit" onClick={handleLogin}>
+            Entrar em sessão
+          </button>
+
+          <div className={styles.registerLink}>
+            <p>Não tem uma conta?</p>
+          </div>
+          <button className={styles.submit}>Inscrever-se</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
