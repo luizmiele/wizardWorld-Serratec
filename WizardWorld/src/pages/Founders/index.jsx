e29ab68b0a347d4d37a4629ece41a554e570960d
luiz miele <luiz.miele@hotmail.com>
2024-06-07 19:23:36 -0300

    CREATE: create founders page

--- WizardWorld/src/pages/Founders/index.jsx
+++ WizardWorld/src/pages/Founders/index.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import styles from './styles.module.css';
-
+import videoSrc from '../../assets/videos/jornal.mp4'
+import gh from '../../assets/images/gh.png'
+import lin from '../../assets/images/in.png'
 const DailyProphet = () => {
   return (
     <div className={styles.principal}>
-      
+      <div className={styles.jornal}>
+        <video src={videoSrc} autoPlay muted />
+        <div className={styles.iconesMiele}>
+          <img src={gh} className={styles.icone} />
+          <img src={lin} className={styles.icone} />
+        </div>
+        <div className={styles.iconesBoeck}>
+          <img src={gh} className={styles.icone} />
+          <img src={lin} className={styles.icone} />
+        </div>
+        <div className={styles.iconesVictor}>
+          <img src={gh} className={styles.icone} />
+          <img src={lin} className={styles.icone} />
+        </div>
+        <div className={styles.iconesCassio}>
+          <img src={gh} className={styles.icone} />
+          <img src={lin} className={styles.icone} />
+        </div>
+        <div className={styles.iconesEnzo}>
+          <img src={gh} className={styles.icone} />
+          <img src={lin} className={styles.icone} />
+        </div>
+        
+      </div>
     </div>
   )
 };
