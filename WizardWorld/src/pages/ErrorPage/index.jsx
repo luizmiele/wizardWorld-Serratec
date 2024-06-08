cfec453ae2cf5b052f7da1f47b5c8ab57ccf7712
luiz miele <luiz.miele@hotmail.com>
2024-06-07 22:22:49 -0300

    feat: create error page

---
+++ WizardWorld/src/pages/ErrorPage/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './styles.module.css'
+import mapa from '../../assets/images/mapa.png'
+
+const ErrorPage = () => {
+  return (
+    <div className={styles.principal}>
+        <div className={styles.t1}>
+            <h2>Parece que você está perdido!</h2>
+            <h2>Essa pagina não existe!</h2>
+        </div>
+
+        <div className={styles.t2}>
+            <h2>Toma esse mapa ai!</h2>
+        </div>
+        <div className={styles.mapa}>
+            /* <img src={mapa}></img> */
+        </div>       
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ WizardWorld/src/pages/Quidditch/index.jsx
@@ -0,0 +1,12 @@
+const Quidditch = () => {
+  
+
+    return (
+        <div className={StyleSheet.principal}>
+           <h2>AQUI VAI TER UMA PAGINA. JURO!</h2>
+            
+        </div>
+    )
+  };
+  
+  export default Quidditch;
