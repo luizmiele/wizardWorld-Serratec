80829aa17ba4b0bf6e26b25bd55e8964ae006680
LucasBoeck23 <162763345+LucasBoeck23@users.noreply.github.com>
2024-06-10 18:17:10 -0300

    feat: Spells

--- WizardWorld/src/pages/Spells/index.jsx
+++ WizardWorld/src/pages/Spells/index.jsx
@@ -1,11 +1,28 @@
 import React, { useState, useEffect } from "react";
 import BackgroundVideo from "../../components/BackgroundVideo/index.jsx";
 import styles from "./styles.module.css";
-import videoSrc from "../../assets/videos/corvinal.mp4";
+import videoSrc from "../../assets/videos/SalaSpells.mp4";
 import centerImage from "../../assets/images/paginaVelha.png";
 import crest from "../../assets/images/casas/hogwarts.png";
+import pena from "../../assets/images/icones/pena.png"
 import studentImage from "../../assets/images/enzo-diretor-moldura.png";
 import { getSpells } from "../../services/Api.js";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const estilo = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const Spell = () => {
   const [showChat, setShowChat] = useState(true);
@@ -20,6 +37,14 @@ const Spell = () => {
   const [effect, setEffect] = useState("");
   const [type, setType] = useState("");
   const [light, setLight] = useState("");
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
+function voltaPagina(){
+  window.history.back();
+}
 
   function mostraDetalhes() {
     const spell = findByName(linkText);
@@ -83,10 +108,32 @@ const Spell = () => {
     <div className={styles.principal}>
       <BackgroundVideo src={videoSrc} />
       <div className={styles.crestContainer}>
-        <a href="http://localhost:5173/gryffindor">
+        <a href="#" onClick={voltaPagina}>
           <img className={styles.crest} src={crest} alt="Crest" />
         </a>
       </div>
+
+      <div className={styles.criarFeitico}> <h2>Crie seu feitiço!</h2>
+        <div className={styles.modal}>
+        <Button onClick={handleOpen}> <img src={pena} alt="criar feitiço" /></Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={estilo}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Modal>
+      </div>
+    </div>
+
       <div className={styles.contentContainer}>
         <div className={styles.paginaContainer}>
           <div
@@ -99,13 +146,14 @@ const Spell = () => {
                 <h2>Qual feitiço está procurando?</h2>
                 <form onSubmit={(e) => e.preventDefault()}>
                   <input
+                  className={styles.formulario}
                     type="text"
                     onChange={handleFiltro}
                     placeholder="Procure seu feitiço..."
                   />
                 </form>
                 {invisivel ? (
-                  <p>NADA</p>
+                  <p></p>
                 ) : (
                   <div className={styles.scrollContainer}>
                     {spellsFiltradas.map((spell) => (
@@ -124,11 +172,15 @@ const Spell = () => {
                 <p>Detalhe INVISIVEL</p>
               ) : (
                 <p>
-                  <span>Nome: </span> {linkText} <br />
-                  <span>Encantamento: </span> {incantation} <br />
-                  <span>Efeito: </span> {effect} <br />
-                  <span>Tipo: </span> {type} <br />
-                  <span>Luz: </span> {light} <br />
+                  <span className={styles.atributo}>Nome: </span> {linkText} <br />
+
+                  <span className={styles.atributo}>Encantamento: </span> {incantation} <br />
+
+                  <span className={styles.atributo}>Efeito: </span> {effect} <br />
+
+                  <span className={styles.atributo}>Tipo: </span> {type} <br />
+
+                  <span className={styles.atributo}>Luz: </span> {light} <br />
                 </p>
               )}
             </div>
@@ -140,13 +192,13 @@ const Spell = () => {
           {showChat && (
             <div className={styles.chatBox}>
               <div className={styles.studentText}>
+                <p>
                 Suas poções favoritas são
-                <ul>
-                  <li>tananan</li>
-                  <li>tananin</li>
-                  <li>trolous</li>
-                  <li>Boeck OO</li>
-                </ul>
+                  <ul>tananan</ul>
+                  <ul>tananin</ul>
+                  <ul>trolous</ul>
+                  <ul>Boeck OO</ul>
+                </p>
               </div>
             </div>
           )}
